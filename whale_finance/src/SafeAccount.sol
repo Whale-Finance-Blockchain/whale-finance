7d987c9af873a6f113f1066087d680a8a99f8b64
Luiz <lj253@gmail.com>
2023-09-18 21:21:28 -0300

    contracts

---
+++ whale_finance/src/SafeAccount.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/interfaces/IERC1271.sol";
+import "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
+
+import "./interface/IERC6551Account.sol";
+
+contract SafeAccount is IERC165, IERC1271, IERC6551Account {
+    receive() external payable {}
+
+    function isValidSignature(
+        bytes32 hash,
+        bytes memory signature
+    ) external view returns (bytes4 magicValue) {
+        bool isValid = SignatureChecker.isValidSignatureNow(
+            owner(),
+            hash,
+            signature
+        );
+
+        if (isValid) {
+            return IERC1271.isValidSignature.selector;
+        }
+
+        return "";
+    }
+
+    function token() public view returns (uint256, address, uint256) {
+        bytes memory footer = new bytes(0x60);
+
+        assembly {
+            extcodecopy(address(), add(footer, 0x20), 0x4d, 0x60)
+        }
+
+        return abi.decode(footer, (uint256, address, uint256));
+    }
+
+    function owner() public view returns (address) {
+        (uint256 chainId, address tokenContract, uint256 tokenId) = token();
+        if (chainId != block.chainid) return address(0);
+
+        return IERC721(tokenContract).ownerOf(tokenId);
+    }
+
+    function _isValidSigner(address signer) internal view returns (bool) {
+        return signer == owner();
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    ) external view override returns (bool) {}
+
+    function executeCall(
+        address to,
+        uint256 value,
+        bytes calldata data
+    ) external payable override returns (bytes memory) {}
+
+    function nonce() external view override returns (uint256) {}
+}
