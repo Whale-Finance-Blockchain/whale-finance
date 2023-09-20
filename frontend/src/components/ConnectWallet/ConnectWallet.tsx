f2f98a1d0ee708ccc28198cb9ff050e095d2e028
Bryan <bryanborck@gmail.com>
2023-09-20 02:25:47 -0300

    little bug fixed

--- frontend/src/components/ConnectWallet/ConnectWallet.tsx
+++ frontend/src/components/ConnectWallet/ConnectWallet.tsx
@@ -8,7 +8,7 @@ export default function ConnectWalletBtn({ isMetamaskInstalled, connectWallet, a
                         onClick={connectWallet}
                         className='w-[48vw] md:w-[24vw] lg:w-[16vw] right-[3vw] p-3 shadow-md font-bold bg-white hover:bg-blue-color text-secondary-color hover:text-white rounded-full cursor-pointer transition duration-1000 ease-in-out'
                     >
-                        <h3 className='flex justify-center'>{signer ? "Wallet Connected: " +
+                        <h3 className='flex justify-center truncate'>{signer ? "Wallet Connected: " +
                             account?.substring(0, 5) +
                             "..." +
                             account?.substring(39, 42) : "Connect Wallet"}</h3>
