bf3e943677dcd8aecaae2996deb4d2c60456fbef
Bryan <bryanborck@gmail.com>
2023-09-20 02:12:29 -0300

    fundid almost done needs integration with firebase

--- frontend/src/components/FormInvestor/FormInvestor.tsx
+++ frontend/src/components/FormInvestor/FormInvestor.tsx
@@ -1,6 +1,6 @@
 export default function FormInvestor(props: any) {
     return (
-        <div className='my-20 mb-12'>
+        <div className='my-8'>
             <div>
                 <div className="mb-4 text-secondary-color">
                     <label className="block font-medium text-xl mb-2" htmlFor="invest">
@@ -13,7 +13,7 @@ export default function FormInvestor(props: any) {
                         placeholder='Qty of ZUSD'
                         value={props.invest}
                         onChange={(e) => props.setInvest(e.target.value)}
-                        className="w-full bg-[white] text-center text-xl text-black p-2 mt-4 rounded-full outline-0 shadow-md hover:bg-gray-100 transition duration-600 ease-in-out focus:ring-2 focus:ring-gray-600 focus:ring-opacity-50 focus:outline-none"
+                        className="w-full bg-[white] text-center text-xl text-black p-2 mt-2 rounded-full outline-0 shadow-md hover:bg-gray-100 transition duration-600 ease-in-out focus:ring-2 focus:ring-gray-600 focus:ring-opacity-50 focus:outline-none"
                     />
                 </div>
             </div>
--- frontend/src/pages/FundId/FundId.tsx
+++ frontend/src/pages/FundId/FundId.tsx
@@ -12,6 +12,7 @@ import { ethers } from 'ethers';
 import { WhaleFinanceAddress, ZusdAddress, scanUrl } from '../../utils/addresses';
 import { QuotaTokenAbi } from '../../contracts/QuotaToken';
 import { WhaleFinanceAbi } from '../../contracts/WhaleFinance';
+import Zusd from '../../assets/zusd.png';
 
 type DataPoint = {
     date: string;
@@ -256,65 +257,76 @@ export default function FundId({ isMetamaskInstalled, connectWallet, account, pr
         <>
             <div className='w-[100vw] h-screen text-gray-700 bg-[#f6f6f6] overflow-y-auto'>
                 <section className="">
-                    <div className="container mx-auto px-0 text-center py-8 md:px-6 lg:px-6">
+                    <div className="container mx-auto px-0 text-center py-8 lg:px-6">
                         <h2 className="flex justify-center items-center bg-white h-[12vh] mx-6 text-4xl font-bold text-center text-secondary-color shadow-lg rounded-[20px]">
                         {fund.name}
                         </h2>
-                        <div className='flex flex-col md:flex-row lg:flex-row justify-center md:h-[70vh] lg:h-[70vh] my-10 mx-6 mb-12 shadow-lg bg-white text-secondary-color rounded-[20px]'>
-                            <div className='flex-1 md:basis-2/3 lg:basis-2/3 md:mx-2 md:px-10 lg:mx-2 lg:px-10'>
-                                <h1 className='font-bold text-2xl mt-6 mb-1 md:text-left md:ml-12 lg:text-left lg:ml-12'>Performance</h1>
-                                <div className='flex justify-center sm:block sm:justify-start lg:block lg:justify-start'>
-                                    <div className='h-[2px] w-36 mb-8 md:ml-12 lg:ml-12 bg-secondary-color'></div>
+                        <div className='flex flex-col lg:flex-row justify-center lg:h-[70vh] my-10 mx-6 mb-12 shadow-lg bg-white text-secondary-color rounded-[20px]'>
+                            <div className='flex-1 lg:basis-2/3 lg:mx-2 lg:px-10'>
+                                <h1 className='font-bold text-2xl mt-6 mb-1 lg:text-left lg:ml-12'>Performance</h1>
+                                <div className='flex justify-center lg:block lg:justify-start'>
+                                    <div className='h-[2px] w-36 mb-8 lg:ml-12 bg-secondary-color'></div>
                                 </div>
-                                <div className='w-[90%] md:w-[100%] lg:w-[100%] h-[80%] flex items-center justify-center'>
+                                <div className='w-[90%] lg:w-[100%] h-[80%] flex items-center justify-center'>
                                     <LineChartComponent data={data} />
                                 </div>
                             </div>
-                            <div className='flex-1 md:basis-1/3 lg:basis-1/3 mx-2 px-10 '>
-                                <h1 className='font-bold text-2xl mt-6 mb-1 md:text-left lg:text-left'>Invest</h1>
-
-
-                                <div className='flex justify-center sm:block sm:justify-start lg:block lg:justify-start'>
-                                    <div className='h-[2px] w-16 md:mb-8 lg:mb-8 bg-secondary-color'></div>
+                            <div className='flex-1 lg:basis-1/3 mx-2 px-10 '>
+                                <h1 className='font-bold text-2xl mt-6 mb-1 lg:text-left'>Invest</h1>
+                                <div className='flex justify-center lg:block lg:justify-start'>
+                                    <div className='h-[2px] w-16 mb-8 bg-secondary-color'></div>
                                 </div>
-
-                                {/* put some data here */}
-
-                                <div className='flex justify-center sm:block sm:justify-start lg:block lg:justify-start'>
-                                        <h3>Your ZUSD Balance: {Number(zusdBalance).toFixed(2)}</h3>
-                                        <h3>Your quotas Balance: {Number(quotaBalance).toFixed(2)}</h3>
-                                        <h3>Quota Price: {Number(quotaPrice).toFixed(2)} USD/quota</h3>
-                                        <h3>Total number of quotas: {Number(totalQuotas).toFixed(2)}</h3>
-                                        <h3
-                                        onClick={() => window.open(`${scanUrl}/address/${quotaAddress}`)}
-                                        style={{
-                                            cursor: "pointer"
-                                        }}
-                                        >See the quota on-chain: {quotaAddress}</h3>
-
+                                <div className='flex flex-col bg-slate-100 p-4 mb-8 rounded-[10px] space-y-1'>
+                                        <div className='grid grid-cols-2'>
+                                            <h3 className='italic'>Your ZUSD Balance:</h3>
+                                            <div className='flex flex-row items-center justify-center'>
+                                                <p className='font-bold text-blue-color'>{Number(zusdBalance).toFixed(2)}</p>
+                                                <img src={Zusd} alt="zusd" className='w-6 h-6 ml-2' />
+                                            </div>
+                                        </div>
+                                        <div className='grid grid-cols-2'>
+                                            <h3 className='italic'>Your quotas Balance:</h3>
+                                            <p className='font-bold text-blue-color'>{Number(quotaBalance).toFixed(2)}</p>
+                                        </div>
+                                        <div className='grid grid-cols-2'>
+                                            <h3 className='italic'>Quota Price:</h3>
+                                            <p className='font-bold text-blue-color'>{Number(quotaPrice).toFixed(2)} USD/quota</p>
+                                        </div>
+                                        <div className='grid grid-cols-2'>
+                                            <h3 className='italic'>Total number of quotas:</h3>
+                                            <p className='font-bold text-blue-color'>{Number(totalQuotas).toFixed(2)}</p>
+                                        </div>
+                                        <div className='grid grid-cols-2'>
+                                            <h3
+                                            className='italic'
+                                            onClick={() => window.open(`${scanUrl}/address/${quotaAddress}`)}
+                                            style={{
+                                                cursor: "pointer"
+                                            }}
+                                            >See the quota on-chain:</h3>
+                                            <p className='font-bold text-blue-color'>{quotaAddress}</p>
+                                        </div>
                                 </div>
-
-
                                 <FormInvestor   
                                     invest={invest}
                                     setInvest={setInvest}
                                 />
                                 <button
-                                className="my-4 bg-gradient-to-r from-blue-color to-secondary-color text-white font-bold rounded-full border-2 border-transparent py-2 px-20 shadow-lg uppercase tracking-wider hover:from-white hover:to-white hover:text-secondary-color hover:border-secondary-color transition duration-1000 ease-in-out" onClick={makeInvestment}
+                                className="mb-4 bg-gradient-to-r from-blue-color to-secondary-color text-white font-bold rounded-full border-2 border-transparent py-2 px-20 shadow-lg uppercase tracking-wider hover:from-white hover:to-white hover:text-secondary-color hover:border-secondary-color transition duration-1000 ease-in-out" onClick={makeInvestment}
                                 >
                                 Invest
                                 </button>
                             </div>
                         </div>
-                        <div className='flex flex-col md:flex-row lg:flex-row justify-center md:h-[70vh] lg:h-[70vh] my-12 mb-24'>
-                            <div className='md:basis-1/2 lg:basis-1/2 mx-6 md:px-10 lg:px-10 h-[70vh] shadow-lg bg-white text-secondary-color rounded-[20px]'>
-                                <h1 className='font-bold text-2xl mt-6 mb-1 mx-10 md:mx-0 lg:mx-0 text-left'>Tokens</h1>
-                                <div className='h-[2px] w-20 mb-8 mx-10 md:mx-0 lg:mx-0 bg-secondary-color'></div>
+                        <div className='flex flex-col lg:flex-row justify-center lg:h-[70vh] my-12 mb-24'>
+                            <div className=' lg:basis-1/2 mx-6 lg:px-10 h-[70vh] shadow-lg bg-white text-secondary-color rounded-[20px]'>
+                                <h1 className='font-bold text-2xl mt-6 mb-1 mx-10 lg:mx-0 text-left'>Tokens</h1>
+                                <div className='h-[2px] w-20 mb-8 mx-10 lg:mx-0 bg-secondary-color'></div>
                                 <div className='w-[100%] h-[80%] flex items-center justify-center'>
                                     <PieChartComponent />
                                 </div>
                             </div>
-                            <div className='mt-12 md:mt-0 lg:mt-0 md:basis-1/2 lg:basis-1/2 mx-6 px-10 h-[70vh] shadow-lg bg-white text-secondary-color rounded-[20px]'>
+                            <div className='mt-12 lg:mt-0 lg:basis-1/2 mx-6 px-10 h-[70vh] shadow-lg bg-white text-secondary-color rounded-[20px]'>
                                 <h1 className='font-bold text-2xl mt-6 mb-1 text-left'>Data</h1>
                                 <div className='h-[2px] w-14 mb-8 bg-secondary-color'></div>
                                 <DataDiv fund={fund}/>
